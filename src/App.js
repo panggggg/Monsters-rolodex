281fa1e125dab3fecf1de42e700cf57d3796a601
panggggg <p.picczy@gmail.com>
2021-08-02 18:10:25 +0700

    Add state and setState at the button

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hello Pang Pawornwan'
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello React
+            {this.state.string}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => this.setState({ string: 'Hello Blue Peeranat' })}>Change Text</button>
         </header>
-      </div>
+      </div >
     )
   }
 }
