dac3fee902c93291c9fc135cd0dd893aff9009aa
panggggg <p.picczy@gmail.com>
2021-08-03 16:14:27 +0700

    Edit in this file

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,5 +4,6 @@ import './card-list.styles.css';
 
 export const CardList = props => {
     console.log(props)
-    return <div className='card-list'>{props.children}</div>;
+    return <div className='card-list'>{props.monsters.map(monsters =>
+        (<h1 key={monsters.id}>{monsters.name}</h1>))}</div>;
 };
