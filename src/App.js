fe909d80eed3753691ba4fba253882ab62c617fa
panggggg <p.picczy@gmail.com>
2021-08-04 16:17:12 +0700

    Add filtering state

--- src/App.js
+++ src/App.js
@@ -20,13 +20,22 @@ class App extends Component {
   }
 
   render() {
+
+    const { monsters, searchField } = this.state;
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
         <input type='search'
           placeholder='search monster'
           onChange={e =>
             this.setState({ searchField: e.target.value })} />
-        <CardList monsters={this.state.monsters}>
+        <CardList monsters={filteredMonsters}>
           {/* <h1>Pawornwan</h1> this is children prop */}
         </CardList>
       </div >
